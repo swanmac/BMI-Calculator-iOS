ee0f34eda84a73d35ea87050ca0d1817ef70e533
Greg Swan <gmswan99@gmail.com>
2023-10-15 17:37:06 -0400

    add background color red

--- BMI Calculator/Controllers/SecondViewController.swift
+++ BMI Calculator/Controllers/SecondViewController.swift
@@ -13,9 +13,12 @@ class SecondViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.backgroundColor = .red
+        
         let label = UILabel()
         label.text = "Hello"
         label.frame = CGRect(x: 0, y: 0, width: 100, height: 50)
+        view.addSubview(label)
     }
     
 }
