4b5a16332bdb6ace7f8dacfc566d3b90c8fe0dfa
Greg Swan <gmswan99@gmail.com>
2023-10-15 14:06:57 -0400

    connect height and weight labels to text output

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -19,11 +19,11 @@ class ViewController: UIViewController {
     }
 
     @IBAction func heightSliderChanged(_ sender: UISlider) {
-        print(String(format: "%.2f", sender.value))
+        heightLabel.text = String(format: "%.2f", sender.value)
     }
     
     @IBAction func weightSliderChanged(_ sender: UISlider) {
-        print(Int(sender.value))
+        weightLabel.text = String(format: "%.0f", sender.value)
     }
 }
 
