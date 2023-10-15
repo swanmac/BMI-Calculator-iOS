9cedf421e6eb98942382f011bd0a5b9f982d28ef
Greg Swan <gmswan99@gmail.com>
2023-10-15 17:32:27 -0400

    design label

--- BMI Calculator/Controllers/SecondViewController.swift
+++ BMI Calculator/Controllers/SecondViewController.swift
@@ -15,6 +15,7 @@ class SecondViewController: UIViewController {
         
         let label = UILabel()
         label.text = "Hello"
+        label.frame = CGRect(x: 0, y: 0, width: 100, height: 50)
     }
     
 }
