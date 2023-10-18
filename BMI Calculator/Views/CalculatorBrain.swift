958c224205f5f349cc62d4c8bfa8849b65a945d6
Greg Swan <gmswan99@gmail.com>
2023-10-18 17:04:34 -0400

    create model CalculatorBrain then struct of same

---
+++ BMI Calculator/Views/CalculatorBrain.swift
@@ -0,0 +1,13 @@
+//
+//  CalculatorBrain.swift
+//  BMI Calculator
+//
+//  Created by Greg Swan on 10/18/23.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct CalculatorBrain {
+    
+}
